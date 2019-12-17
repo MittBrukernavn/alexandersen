61acbac1d2571c229d383004ae76e7e8f440309c
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2019-12-17 16:31:16 +0100

    Added more minesweeper logic

--- client/src/components/minesweeper/Minesweeper.jsx
+++ client/src/components/minesweeper/Minesweeper.jsx
@@ -1,41 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import MinesweeperTile from './MinesweeperTile.jsx';
 
 const Wrapper = styled.div`
+`;
 
+const Table = styled.table`
+  margin: auto;
 `;
 
 const Minesweeper = (props) => {
   const [ initialized, setInitialized ] = useState(false);
-  const [ board, setBoard ] = useState([[{bomb: false, checked: false}, {bomb: false, checked: false}], [{bomb: false, checked: false}, {bomb: false, checked: false}]]);
+  const [ board, setBoard ] = useState([[]]);
 
   const clicked = (rowNumber, colNumber) => {
+    const nextBoard = [...board];
     if (!initialized) {
       setInitialized(true);
-      // set bombs
-
+      // set bombs in accordance to the first click
+      const bombCount = 20;
+      // generate all eligible bomb points
+      const possiblePositions = [];
+      for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[i].length; j++) {
+          if (i !== rowNumber || j !== colNumber) {
+            possiblePositions.push([i, j]);
+          }
+        }
+      }
+      // TODO: randomly shuffle
+      
+      // pick the first positions of the randomized list, and set them to be bombs
+      for(let i = 0; i < bombCount; i++) {
+        const [y, x] = possiblePositions[i];
+        nextBoard[y][x].bomb = true;
+        for(let m = -1; m <= 1; m++) {
+          for(let n = -1; n <=1; n++) {
+            const ny = y + m;
+            const nx = x + n;
+            if (ny >= 0 && ny < nextBoard.length && nx >= 0 && nx < nextBoard[ny].length) {
+              nextBoard[y+m][x+n].num++;
+            }
+          }
+        }
+      }
       console.log('Initialized');
     }
+    nextBoard[rowNumber][colNumber].checked = true;
+    setBoard(nextBoard);
   };
+  
+  useEffect(()=>{
+    const rowCount = 10;
+    const colCount = 10;
+    const newBoard = [];
+    for(let i = 0; i < rowCount; i++) {
+      const thisRow = [];
+      for(let j = 0; j < colCount; j++) {
+        thisRow.push({bomb: false, checked: false, num: 0});
+      }
+      newBoard.push(thisRow);
+    }
+    setBoard(newBoard);
+  }, []);
 
   return (
-    <table>
-      <tbody>
-        {
-          board.map((row, rowNumber) => (
-            <tr key={rowNumber}>
-              {row.map((tile, colNumber) => {
-                const { bomb, checked } = tile;
-                return <MinesweeperTile key={colNumber} bomb={bomb} tested={checked} onClick={()=>clicked(rowNumber, colNumber)} />;
-              })}
-            </tr>
-          ))
+    <Wrapper>
+      <Table>
+        <tbody>
+          {
+            board.map((row, rowNumber) => (
+              <tr key={rowNumber}>
+                {row.map((tile, colNumber) => {
+                  const { bomb, checked, num } = tile;
+                  return <MinesweeperTile key={colNumber} bomb={bomb} tested={checked} num={num} onClick={()=>clicked(rowNumber, colNumber)} />;
+                })}
+              </tr>
+            ))
 
-        }
-      </tbody>
-    </table>
+          }
+        </tbody>
+      </Table>
+    </Wrapper>
   );
 }
 
--- client/src/components/minesweeper/MinesweeperTile.jsx
+++ client/src/components/minesweeper/MinesweeperTile.jsx
@@ -5,14 +5,18 @@ const Wrapper = styled.td`
   width: 2em;
   height: 2em;
   border: 2px solid black;
-  background-color: ${props => props.tested ? 'blue' : 'white' };
+  background-color: ${props => props.tested ? 'cyan' : 'white' };
   ${props => props.bomb && props.tested ? 'background-color: red;' : null}
 `;
 
 const MinesweeperTile = props => {
-  const { tested, bomb, onClick } = props;
+  const { tested, bomb, num, onClick } = props;
 
-  return <Wrapper tested={tested} bomb={bomb} onClick={onClick} />;
+  return (
+    <Wrapper tested={tested} bomb={bomb} onClick={onClick}>
+      {tested && !bomb && num > 0 ? num : null }
+    </Wrapper>
+  );
 }
 
 export default MinesweeperTile;
