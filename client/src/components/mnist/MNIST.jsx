251f1d1fed43c720262cd79b780c201a0c1d98d5
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-06-06 14:54:58 +0200

    Added ML model to the project

--- client/src/components/mnist/MNIST.jsx
+++ client/src/components/mnist/MNIST.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import * as tf from '@tensorflow/tfjs';
 
 import DrawingCanvas from './DrawingCanvas'
 
@@ -8,16 +9,52 @@ const Wrapper = styled.div`
 `;
 
 const MNIST = () => {
+  const [model, setModel] = useState(null);
+  const [pixels, setPixels] = useState([]);
+  const [digit, setDigit] = useState(-1);
+  const [probability, setProbability] = useState(0);
+
+  useEffect(() => {
+    const internal = async () => {
+      const m = await tf.loadLayersModel('/tfjs-mnist/model.json');
+      console.log(m);
+      setModel(m);
+    };
+    internal();
+  }, []);
+
+  useEffect(() => {
+    if (model && pixels.length) {
+      const prediction = model.predict(tf.tensor(pixels, [1, 784]));
+      prediction.data().then((d) => {
+        const prob = Math.max(...d);
+        const dig = d.indexOf(prob);
+        setDigit(dig);
+        setProbability(prob);
+      });
+    }
+  }, [model, pixels])
+
+  let message;
+  if (digit >= 0) {
+    message = `My model is ${probability*100}% certain you wrote a ${digit}.`;
+  } else if (model) {
+    message = 'Start drawing and I\'ll try to read it!';
+  } else {
+    message = 'Model is loading, shouldn\'t take too long';
+  }
+
   return (
     <Wrapper>
       <h1>MNIST-project</h1>
       <p>
-        Under construction.
-        The idea is basically to allow people to write a number, and the neural net will attempt to read it.
-        Sounds simple enough, right?
-        Let's hope it is.
+        Write a number here. Hopefully, the model is be able to read it.
+        If not, my excuse is that I am new to this.
       </p>
-      <DrawingCanvas updatePixels={console.log} />
+      <DrawingCanvas updatePixels={setPixels} />
+      <p>
+        {message}
+      </p>
     </Wrapper>
   );
 };
