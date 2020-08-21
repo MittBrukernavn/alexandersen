7e22605c98cd1a56b0c27ae0e55874c154a2c6f3
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-08-21 15:22:46 +0200

    Change model

--- client/src/components/neural-nets/yelp/Yelp.jsx
+++ client/src/components/neural-nets/yelp/Yelp.jsx
@@ -15,15 +15,16 @@ const Yelp = () => {
     const res = await fetch(`/pyapi/yelp/${review}`);
     const t = await res.text();
     const prob = parseFloat(t);
+    console.log(t);
     if (prob < 0.1) {
       setVerdict(`Very negative review (${t})`);
     } else if (prob < 0.5) {
       setVerdict(`Somewhat negative review (${t})`);
-    } else if (prob < 0.9) {
-      setVerdict(`Somewhat positive review (${t})`);
-    } else if (t === '0.9714157581329346') {
+    } else if (t === '0.6575687527656555') {
       setVerdict(`Very positive review (${t}). WARNING: This probability is indicative
         that the words you chose are not in the model's vocabulary.`);
+    } else if (prob < 0.9) {
+      setVerdict(`Somewhat positive review (${t})`);
     } else {
       setVerdict(`Very positive review (${t})`);
     }
