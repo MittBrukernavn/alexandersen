0a0bb6ebee4513b76c0b580a61c39ca2ccbd6bdc
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-10-06 20:21:27 +0200

    Fixed room structure

--- client/src/components/dotmocracy/Dotmocracy.jsx
+++ client/src/components/dotmocracy/Dotmocracy.jsx
@@ -3,19 +3,19 @@ import io from 'socket.io-client';
 
 import Background from '../general/Background';
 
+// declare this outside the component to avoid it re-connecting on any function call
+let socket;
+
 const Dotmocracy = () => {
-  const [postits, setPostits] = useState([]);
+  // const [postits, setPostits] = useState([]);
   const [messages, setMessages] = useState([]);
   const [name, setName] = useState('');
-  const socket = io('/dotmocracy');
 
   useEffect(() => {
-    socket.on('init_postits', (p) => {
-      setPostits((prev) => [...prev, ...p]);
-    });
+    socket = io('/dotmocracy');
+    window.io = socket;
 
     socket.on('msg', (msg) => {
-      console.log(msg);
       setMessages((prev) => [...prev, msg]);
     });
   }, []);
@@ -25,12 +25,15 @@ const Dotmocracy = () => {
       <input type="text" onChange={(e) => setName(e.target.value)} />
       <button type="button" onClick={() => socket.emit('name', name)}>Register name</button>
       <button type="button" onClick={() => socket.emit('room', 'TMM4220')}>Join room</button>
-      {postits.map(({ id, text, dots }) => (
+      {/* postits.map(({ id, text, dots }) => (
         <p key={id}>
           {`${text}, ${dots.length} votes`}
         </p>
+      )) */}
+      {messages.map((msg, i) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <p key={i}>{msg}</p>
       ))}
-      {messages.map((msg) => <p>{msg}</p>)}
     </Background>
   );
 };
