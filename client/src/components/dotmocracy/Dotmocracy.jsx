eee4aa8507609ac3c63cdc2af752bc50b52e34cb
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-10-06 17:40:24 +0200

    Fledged out socket.io-structure with rooms and names

--- client/src/components/dotmocracy/Dotmocracy.jsx
+++ client/src/components/dotmocracy/Dotmocracy.jsx
@@ -4,18 +4,33 @@ import io from 'socket.io-client';
 import Background from '../general/Background';
 
 const Dotmocracy = () => {
+  const [postits, setPostits] = useState([]);
   const [messages, setMessages] = useState([]);
-  const socket = io();
+  const [name, setName] = useState('');
+  const socket = io('/dotmocracy');
 
   useEffect(() => {
-    socket.on('message', (s) => {
-      setMessages((msgs) => [...msgs, s]);
+    socket.on('init_postits', (p) => {
+      setPostits((prev) => [...prev, ...p]);
+    });
+
+    socket.on('msg', (msg) => {
+      console.log(msg);
+      setMessages((prev) => [...prev, msg]);
     });
   }, []);
 
   return (
     <Background>
-      {messages.map((msg) => <p>{ msg }</p>)}
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <button type="button" onClick={() => socket.emit('name', name)}>Register name</button>
+      <button type="button" onClick={() => socket.emit('room', 'TMM4220')}>Join room</button>
+      {postits.map(({ id, text, dots }) => (
+        <p key={id}>
+          {`${text}, ${dots.length} votes`}
+        </p>
+      ))}
+      {messages.map((msg) => <p>{msg}</p>)}
     </Background>
   );
 };
