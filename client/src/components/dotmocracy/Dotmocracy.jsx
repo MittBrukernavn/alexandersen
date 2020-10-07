3db3db258589269d2aa58a62d5258de61c0074c6
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-10-07 11:39:07 +0200

    Made dotmocracy component more stateful

--- client/src/components/dotmocracy/Dotmocracy.jsx
+++ client/src/components/dotmocracy/Dotmocracy.jsx
@@ -1,38 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-import styled from 'styled-components';
 
 import Background from '../general/Background';
+import Body from '../general/Body';
 import Members from './Members';
 
-const Grid = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  flex: 1;
-  display: grid;
-  grid-template-columns: 1em 1fr 1fr 1em;
-  grid-template-rows: 1em 1fr 1fr 1em;
-  grid-template-areas:
-    ". . . ."
-    ". topleft topright ."
-    ". botleft botright ."
-    ". . . ."
-`;
-
-const GridItem = styled.div`
-  grid-area: ${(props) => props.position};
-`;
 
 // declare this outside the component to avoid it re-connecting on any function call
 let socket;
 
 const Dotmocracy = () => {
   // const [postits, setPostits] = useState([]);
+  const [phase, setPhase] = useState('choose name');
+  const [name, setName] = useState('');
+  const [roomName, setRoomName] = useState('TMM4220');
   const [postits, setPostits] = useState([]);
   const [members, setMembers] = useState([]);
   const [messages, setMessages] = useState([]);
   const [roomInfo, setRoomInfo] = useState({});
-  const [name, setName] = useState('');
+
+  // creates a callback for setting the phase if the parameter is set to true
+  const setPhaseOnSuccess = (newPhase) => (success) => {
+    if (success) {
+      setPhase(newPhase);
+    }
+  };
 
   useEffect(() => {
     socket = io('/dotmocracy');
@@ -61,30 +53,46 @@ const Dotmocracy = () => {
       setMembers((prev) => prev.filter((user) => userId !== user.userId));
     });
   }, []);
-
+  if (phase === 'choose name') {
+    return (
+      <Background>
+        <Body>
+          <h2>What is your name?</h2>
+          <input type="text" onChange={(e) => setName(e.target.value)} />
+          <button type="button" onClick={() => socket.emit('name', name, setPhaseOnSuccess('choose room'))}>
+            Register
+          </button>
+        </Body>
+      </Background>
+    );
+  }
+  if (phase === 'choose room') {
+    return (
+      <Background>
+        <Body>
+          <h2>Enter a room to join</h2>
+          <input type="text" value={roomName} onChange={(e) => setRoomName(e.target.value)} />
+          <button type="button" onClick={() => socket.emit('room', 'TMM4220', setPhaseOnSuccess('room'))}>
+            Join room
+          </button>
+        </Body>
+      </Background>
+    );
+  }
   return (
     <Background>
-      <Grid>
-        <GridItem position="topleft">
-          <input type="text" onChange={(e) => setName(e.target.value)} />
-          <button type="button" onClick={() => socket.emit('name', name)}>Register name</button>
-          <button type="button" onClick={() => socket.emit('room', 'TMM4220')}>Join room</button>
-        </GridItem>
-        {/* postits.map(({ id, text, dots }) => (
-          <p key={id}>
-            {`${text}, ${dots.length} votes`}
-          </p>
-        )) */}
-        <GridItem position="botleft">
-          {messages.map((msg, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <p key={i}>{msg}</p>
-          ))}
-        </GridItem>
-        <GridItem position="topright">
-          <Members members={members} decider={roomInfo.decider} />
-        </GridItem>
-      </Grid>
+      <Body>
+        <Members members={members} decider={roomInfo.decider} />
+        {messages.map((msg, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <p key={i}>{msg}</p>
+        ))}
+      </Body>
+      {/* postits.map(({ id, text, dots }) => (
+        <p key={id}>
+          {`${text}, ${dots.length} votes`}
+        </p>
+      )) */}
     </Background>
   );
 };
--- client/src/components/dotmocracy/Members.jsx
+++ client/src/components/dotmocracy/Members.jsx
@@ -2,17 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+text-align: left;
+margin: auto;
+`;
+
 const Decider = styled.span`
 color: red;
 `;
 
 const Ul = styled.ul`
-  text-align: left;
+margin: auto;
+text-align: left;
+`;
+
+const H2 = styled.h2`
+margin: 0px  0px;
 `;
 
 const Members = ({ members, decider }) => (
-  <div>
-    <h2>Connected users: </h2>
+  <Wrapper>
+    <H2>Connected users: </H2>
     <Ul>
       {members.map(({ userId, name, votesCast }) => (
         <li key={userId}>
@@ -22,7 +32,7 @@ const Members = ({ members, decider }) => (
         </li>
       ))}
     </Ul>
-  </div>
+  </Wrapper>
 );
 
 Members.propTypes = {
