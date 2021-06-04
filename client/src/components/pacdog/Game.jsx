fff202e4a291b6775c6dcbcd33db71022e3001c0
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2021-06-04 12:57:54 +0200

    Pacdog skeleton code and todos

--- client/src/components/pacdog/Game.jsx
+++ client/src/components/pacdog/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import useWindowDimensions from '../../utils/windowDimensions';
@@ -15,9 +15,38 @@ flex-flow: column nowrap;
 
 const Game = () => {
   const { width, height } = useWindowDimensions();
-  // eslint-disable-next-line
-  const [direction, setDirection] = useState(null);
 
+  // TODO: make a board (and graphics for it)
+  // eslint-disable-next-line no-unused-vars
+  const [board, setBoard] = useState(null);
+
+  // eslint-disable-next-line no-unused-vars
+  const tick = (prevBoard) => {
+    // choose ghost direction (BFS?) and move them
+    // move pacman
+    // if pacman collides with a ghost: game over (or send ghost to spawn with "timeout" period)
+    // if any dots are picked up: remove it, update score/state
+    //   if no dots remain, level complete (reset or consider it a win?)
+    // return board
+  };
+
+  useEffect(() => {
+    // TODO: handle inactive game states (e.g. before game starts or a paused game)
+    const a = setInterval(() => setBoard(tick), 50);
+    return () => clearInterval(a);
+  }, []);
+
+  // TODO: add controls
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      // eslint-disable-next-line no-unused-vars
+      const { keycode } = e;
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  });
+
+  // TODO: button controls for touch screens
   if (width < height) {
     return (
       <Column>
